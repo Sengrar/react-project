2f0567ecf3f30138ccda53ba79b1fc830befce26
Sengrar <95856738+Sengrar@users.noreply.github.com>
2026-02-15 15:18:56 +0530

    create and add basic react app using vite utility

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <p className="read-the-docs">
+        Basic react project with Vite
+      </p>
+    </>
+  )
+}
+
+export default App
