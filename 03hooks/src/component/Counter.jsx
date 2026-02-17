ad4b0f4f71c9572af3c9d448992a47d5edcc11ce
Sengrar <95856738+Sengrar@users.noreply.github.com>
2026-02-18 00:14:56 +0530

    modify: add decrese and reset button in 03hooks/

--- 03hooks/src/component/Counter.jsx
+++ 03hooks/src/component/Counter.jsx
@@ -6,7 +6,9 @@ const Counter = ()=>{
     return(
         <>
           <h1>{count}</h1>
-          <button onClick={()=>setCount(count + 1)}>Increase</button>
+          <button onClick={()=>setCount(count + 1)}>Increase {count}</button>
+          <button onClick={()=>{if(count>0){setCount(count - 1)}}}>Decrease {count}</button>
+          <button onClick={()=>setCount(0)}>Reset {count}</button>
         </>
     )
 }
